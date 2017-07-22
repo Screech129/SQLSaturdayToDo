920a6fdee85fd3148f6d2efff65739a6f3f18a97
David Silver <david.silver@sparkhound.com>
2017-07-22 15:55:52 -0500

    Cleaned up Startup to account for Migrations and added migrations

--- SqlSaturdayToDo.Backend/App_Start/Startup.MobileApp.cs
+++ SqlSaturdayToDo.Backend/App_Start/Startup.MobileApp.cs
@@ -9,6 +9,7 @@ using Microsoft.Azure.Mobile.Server.Config;
 using SqlSaturdayToDo.Backend.DataObjects;
 using SqlSaturdayToDo.Backend.Models;
 using Owin;
+using System.Data.Entity.Migrations;
 
 namespace SqlSaturdayToDo.Backend
 {
@@ -18,49 +19,19 @@ namespace SqlSaturdayToDo.Backend
         {
             HttpConfiguration config = new HttpConfiguration();
 
-            new MobileAppConfiguration()
-                .UseDefaultConfiguration()
-                .ApplyTo(config);
+            var mobileConfig = new MobileAppConfiguration();
+
+            mobileConfig.AddTablesWithEntityFramework()
+            .ApplyTo(config);
 
             // Use Entity Framework Code First to create database tables based on your DbContext
-            Database.SetInitializer(new MobileServiceInitializer());
-
-            MobileAppSettingsDictionary settings = config.GetMobileAppSettingsProvider().GetMobileAppSettings();
-
-            if (string.IsNullOrEmpty(settings.HostName))
-            {
-                app.UseAppServiceAuthentication(new AppServiceAuthenticationOptions
-                {
-                    // This middleware is intended to be used locally for debugging. By default, HostName will
-                    // only have a value when running in an App Service application.
-                    SigningKey = ConfigurationManager.AppSettings["SigningKey"],
-                    ValidAudiences = new[] { ConfigurationManager.AppSettings["ValidAudience"] },
-                    ValidIssuers = new[] { ConfigurationManager.AppSettings["ValidIssuer"] },
-                    TokenHandler = config.GetAppServiceTokenHandler()
-                });
-            }
+            var migrator = new DbMigrator(new Migrations.Configuration());
+            migrator.Update();
 
             app.UseWebApi(config);
         }
     }
 
-    public class MobileServiceInitializer : CreateDatabaseIfNotExists<MobileServiceContext>
-    {
-        protected override void Seed(MobileServiceContext context)
-        {
-            List<TodoItem> todoItems = new List<TodoItem>
-            {
-                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "First item", Complete = false },
-                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Second item", Complete = false }
-            };
-
-            foreach (TodoItem todoItem in todoItems)
-            {
-                context.Set<TodoItem>().Add(todoItem);
-            }
-
-            base.Seed(context);
-        }
-    }
+   
 }
 
