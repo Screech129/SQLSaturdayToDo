3e115b1f79eb23d2149d4765f6f620105d820c0e
David Silver <david.silver@sparkhound.com>
2017-07-22 14:39:04 -0500

    Add project files.

---
+++ SqlSaturdayToDo.Backend/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SqlSaturdayToDo.Backend.Startup))]
+
+namespace SqlSaturdayToDo.Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
